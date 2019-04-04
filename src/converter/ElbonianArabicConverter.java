012d33336c1de9efa6a20a1ac57c0f24c0e8a8a7
larapadir <lpadir@wpi.edu>
2019-04-04 15:47:16 -0400

    code

--- src/converter/ElbonianArabicConverter.java
+++ src/converter/ElbonianArabicConverter.java
@@ -13,6 +13,8 @@ public class ElbonianArabicConverter {
 
     // A string that holds the number (Elbonian or Arabic) you would like to convert
     private final String number;
+    private boolean isNumbers;
+
 
     /**
      * Constructor for the ElbonianArabic class that takes a string. The string should contain a valid
@@ -30,7 +32,127 @@ public class ElbonianArabicConverter {
     public ElbonianArabicConverter(String number) throws MalformedNumberException, ValueOutOfBoundsException {
 
         // TODO check to see if the number is valid, then set it equal to the string
+        if (number.length() < 1) throw new ValueOutOfBoundsException(number);
+        number = number.trim();
         this.number = number;
+
+        this.isNumbers =true;
+        for( char c : number.toCharArray()){
+            if(! Character.isDigit(c) ){
+                this.isNumbers = false;
+                break;
+            }
+        }
+        if(this.isNumbers == false) {
+            for (char c : number.toCharArray()) {
+                if (!((c == 'M') || (c == 'C') || (c == 'X') || (c == 'I') || (c == 'N') || (c == 'D') || (c == 'Y') || (c == 'J'))) {
+                    throw new MalformedNumberException(number);
+                }
+
+            }
+
+            int count_N = number.chars().filer(c -> c == 'N').count();
+            int count_M = number.chars().filer(c -> c == 'M').count();
+            int count_D = number.chars().filer(c -> c == 'D').count();
+            int count_C = number.chars().filer(c -> c == 'C').count();
+            int count_Y = number.chars().filer(c -> c == 'Y').count();
+            int count_X = number.chars().filer(c -> c == 'X').count();
+            int count_J = number.chars().filer(c -> c == 'J').count();
+            int count_I = number.chars().filer(c -> c == 'I').count();
+
+            if((count_M > 2) || (count_C > 2) ||
+                    (count_X > 2) || (count_I > 2)
+                || (count_N > 3) || (count_D > 3)
+                || (count_Y > 3) || (count_J > 3)) {
+                throw new MalformedNumberException(number);
+            }
+
+            if ((count_N >= 3 && count_M > 0)
+                || (count_D >= 3 && count_C > 0)
+                || (count_Y >= 3 && count_X > 0)
+                || (count_J >= 3 && count_I > 0)) {
+                throw new MalformedNumberException(number);
+            }
+
+
+
+            // N M N
+
+
+            // we will have problems, because if the character is not in the string, it returns -1
+
+            // PROBLEM SOLVED
+
+
+            int min_position_M = this.number.indexOf('M');
+            int min_position_D = this.number.indexOf('D');
+            int min_position_C = this.number.indexOf('C');
+            int min_position_Y = this.number.indexOf('Y');
+            int min_position_X = this.number.indexOf('X');
+            int min_position_J = this.number.indexOf('J');
+            int min_position_I = this.number.indexOf('I');
+            int max_position_N = this.number.lastIndexOf('N');
+            int max_position_M = this.number.lastIndexOf('M');
+            int max_position_D = this.number.lastIndexOf('D');
+            int max_position_C = this.number.lastIndexOf('C');
+            int max_position_Y = this.number.lastIndexOf('Y');
+            int max_position_X = this.number.lastIndexOf('X');
+            int max_position_J = this.number.lastIndexOf('J');
+
+            if (min_position_M == -1) {
+                min_position_M = this.number.length() + 1;
+            }
+            if (min_position_D == -1) {
+                min_position_D = this.number.length() + 1;
+            }
+            if (min_position_C == -1) {
+                min_position_C = this.number.length() + 1;
+            }
+            if (min_position_Y == -1) {
+                min_position_Y = this.number.length() + 1;
+            }
+            if (min_position_X == -1) {
+                min_position_X = this.number.length() + 1;
+            }
+            if (min_position_J == -1) {
+                min_position_J = this.number.length() + 1;
+            }
+            if (min_position_I == -1) {
+                min_position_I = this.number.length() + 1;
+            }
+
+            if (min_position_M < max_position_N || min_position_D < max_position_N || min_position_C < max_position_N
+                    || min_position_Y < max_position_N || min_position_X < max_position_N || min_position_J < max_position_N
+                    || min_position_I < max_position_N) throw new MalformedNumberException(number);
+            if (min_position_D < max_position_M || min_position_C < max_position_M
+                    || min_position_Y < max_position_M || min_position_X < max_position_M || min_position_J < max_position_M
+                    || min_position_I < max_position_M) throw new MalformedNumberException(number);
+            if (min_position_C < max_position_D
+                    || min_position_Y < max_position_D || min_position_X < max_position_D || min_position_J < max_position_D
+                    || min_position_I < max_position_D) throw new MalformedNumberException(number);
+            if (min_position_Y < max_position_C || min_position_X < max_position_C || min_position_J < max_position_C
+                    || min_position_I < max_position_C) throw new MalformedNumberException(number);
+            if (min_position_X < max_position_Y || min_position_J < max_position_Y
+                    || min_position_I < max_position_Y) throw new MalformedNumberException(number);
+            if (min_position_J < max_position_X || min_position_I < max_position_X) throw new MalformedNumberException(number);
+            if (min_position_I < max_position_J) throw new MalformedNumberException(number);
+
+
+            // if N appears 3 times, M cannot be used
+            // ...
+
+
+
+
+
+        }
+
+
+        if (this.toArabic() <= 0 || this.toArabic() > 9999) throw new ValueOutOfBoundsException(number);
+
+
+
+
     }
 
     /**
@@ -40,8 +162,33 @@ public class ElbonianArabicConverter {
      * @return An arabic value
      */
     public int toArabic() {
-        // TODO Fill in the method's body
-        return 1;
+        if (this.isNumbers) {
+            return Integer.parseInt(this.number);
+        } else {
+            // do stuff
+            // divide things by numbers and get remainders
+            int result = 0;
+
+            int count_N = number.chars().filer(c -> c == 'N').count();
+            int count_M = number.chars().filer(c -> c == 'M').count();
+            int count_D = number.chars().filer(c -> c == 'D').count();
+            int count_C = number.chars().filer(c -> c == 'C').count();
+            int count_Y = number.chars().filer(c -> c == 'Y').count();
+            int count_X = number.chars().filer(c -> c == 'X').count();
+            int count_J = number.chars().filer(c -> c == 'J').count();
+            int count_I = number.chars().filer(c -> c == 'I').count();
+
+            result += count_N * 3000;
+            result += count_M * 1000;
+            result += count_D * 300;
+            result += count_C * 100;
+            result += count_Y * 30;
+            result += count_X * 10;
+            result += count_J * 3;
+            result += count_I * 1;
+
+            return result;
+        }
     }
 
     /**
@@ -51,7 +198,62 @@ public class ElbonianArabicConverter {
      */
     public String toElbonian() {
         // TODO Fill in the method's body
-        return "I";
+        if (!this.isNumbers) {
+            return this.number;
+        } else {
+            StringBuilder result = new StringBuilder();
+
+            int countN = Integer.parseInt(this.number) / 3000;
+            int remainder = Integer.parseInt(this.number) % 3000;
+            for(int i = 0; i < countN; i++){
+                result.append("N");
+            }
+
+            int countM = remainder / 1000;
+            remainder = remainder % 1000;
+            for(int i = 0; i < countM; i++){
+                result.append("M");
+            }
+
+            int countD = remainder / 300;
+            remainder = remainder % 300;
+            for(int i = 0; i < countD; i++){
+                result.append("D");
+            }
+
+            int countC = remainder / 100;
+            remainder = remainder % 100;
+            for(int i = 0; i < countC; i++){
+                result.append("C");
+            }
+
+            int countY = remainder / 30;
+            remainder = remainder % 30;
+            for(int i = 0; i < countY; i++){
+                result.append("Y");
+            }
+
+            int countX = remainder / 10;
+            remainder = remainder % 10;
+            for(int i = 0; i < countX; i++){
+                result.append("X");
+            }
+
+            int countJ = remainder / 3;
+            remainder = remainder % 3;
+            for(int i = 0; i < countJ; i++){
+                result.append("J");
+            }
+
+            int countI = remainder / 1;
+            remainder = remainder % 1;
+            for(int i = 0; i < countI; i++){
+                result.append("I");
+            }
+
+         return result.toString();
+        }
+
     }
 
 }
--- src/converter/tests/ConverterTests.java
+++ src/converter/tests/ConverterTests.java
@@ -16,12 +16,24 @@ public class ConverterTests {
     public void ElbonianToArabicSampleTest() throws MalformedNumberException, ValueOutOfBoundsException {
         ElbonianArabicConverter converter = new ElbonianArabicConverter("1");
         assertEquals(converter.toElbonian(), "I");
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("NNNDDDYYYJJJ");
+        assertEquals("9999", converter.toElbonian());
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("NMJI");
+        assertEquals("4004", converter.toElbonian());
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("NNMDYYYJI");
+        assertEquals("7394", converter.toElbonian());
     }
 
     @Test
     public void ArabicToElbonianSampleTest() throws MalformedNumberException, ValueOutOfBoundsException {
         ElbonianArabicConverter converter = new ElbonianArabicConverter("I");
         assertEquals(converter.toArabic(), 1);
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("4004");
+        assertEquals("NMJI", converter.toElbonian());
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("9999");
+        assertEquals("NNNDDDYYYJJJ", converter.toElbonian());
+        ElbonianArabicConverter converter = new ElbonianArabicConverter("7394");
+        assertEquals("NNMDYYYJI", converter.toElbonian());
     }
 
     @Test(expected = MalformedNumberException.class)
@@ -29,10 +41,80 @@ public class ConverterTests {
         throw new MalformedNumberException("TEST");
     }
 
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("MNJJ");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("DMJJ");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("CDJJ");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("YCJJ");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("XYJJ");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("JXII");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("IJJ");
+    }
+
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("9 9");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("N M");
+    }
+
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("DDDC");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("NNNM");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("YYYX");
+    }
+
+    @Test(expected = MalformedNumberException.class)
+    public void malformedNumberTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new MalformedNumberException("JJJI");
+    }
+
     @Test(expected = ValueOutOfBoundsException.class)
     public void valueOutOfBoundsTest() throws MalformedNumberException, ValueOutOfBoundsException {
         throw new ValueOutOfBoundsException("0");
     }
 
-    // TODO Add more test cases
+    @Test(expected = ValueOutOfBoundsException.class)
+    public void valueOutOfBoundsTest() throws MalformedNumberException, ValueOutOfBoundsException {
+        throw new ValueOutOfBoundsException("12000");
+    }
 }
